a6259f53a921906d261d278e741c315c01d78695
ABEL <abelovearn@gmail.com>
2023-05-21 11:21:47 +0300

    made homework-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -31,3 +34,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        try:
+            if len(name) < 10:
+                self.__name = name
+        except Exception:
+            print('Длина наименования товара превышает 10 символов')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        all = []
+
+        with open('items.csv',  encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                item = cls(row['name'], row['price'], row['quantity'])
+                all.append(item)
+
+    @staticmethod
+    def string_to_number(str_number):
+        return int(float(str_number))
