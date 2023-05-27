2038804ff9924b88b7a5f4e4ed61ddf80f0baf14
ABEL <abelovearn@gmail.com>
2023-05-27 11:47:03 +0300

    made homework-3

--- tests/test_item.py
+++ tests/test_item.py
@@ -25,4 +25,19 @@ def test_all(item1):
 
 def test_name():
     with pytest.raises(ValueError):
-        len('СуперСмартфон')
+        len('СуперСмартфон Apple14')
+
+
+def test_instantiate_from_csv() -> None:
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 10
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
