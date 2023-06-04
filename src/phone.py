6eb5cb659b243d29027acee24c1bab608a6624f2
ABEL <abelovearn@gmail.com>
2023-06-05 00:19:57 +0300

    fix __number_of_sim homework-4

--- src/phone.py
+++ src/phone.py
@@ -8,7 +8,7 @@ class Phone(Item):
         # Вызываем метод базового класса
         super().__init__(name, price, quantity)
         # Дополнительный атрибут
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
